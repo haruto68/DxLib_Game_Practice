216985f185ddc1231167a118d4b82936f5c6d1b3
haruto68 <138412489+haruto68@users.noreply.github.com>
2023-10-17 11:11:56 +0900

    10/17
    FreamControl.cpp
    FreamControl.h
    GameMainScene.cpp
    GameMainScene.h
    InputControl.cpp
    InputControl.h
    終了

--- GamePrograming/Tetris/GameMainScene.h
+++ GamePrograming/Tetris/GameMainScene.h
@@ -9,11 +9,10 @@
 ***********************************************/
 
 /***********************************************
-* プロトタイプ宣言
+* プロトタイプ宣言宣言
 ***********************************************/
-int Block_initialze(void);		//初期化処理
-void Block_Update(void);		//更新処理
-void Block_Draw(void);			//描画処理
+int GameMainScene_Initialize(void);
+void GameMainScene_Update(void);
+void GameMainScene_Draw(void);
 
-int Get_GenerateFlg(void);		//ブロックの生成判定処理
-int Get_Line(void);				//消したラインの数取得処理
+int Get_Score(void);
